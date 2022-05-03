42b4a3be1b28b6d3b8a482c11872c640808db9a8
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-02 19:07:23 -0500

    Se creo app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const Trello = require("./main.js");
+
+if(!process.env.KEY && !process.env.TOKEN) {
+  throw new Error("No existe el api key ni el token");
+}
+
+const trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+var cardTitle = `Card Nueva ${new Date()}`;
+console.log("Ejecutando!");
+
+trello.addCard(cardTitle, "LaunchX Card Description", "62704a8959513806c62c0fbc",
+  function (error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });
